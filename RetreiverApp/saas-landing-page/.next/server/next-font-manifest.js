4753f7d5161c4c5e85c32a0a2c009e24bdbf7eea
Asufian1 <122128186+Asufian1@users.noreply.github.com>
2025-04-03 14:53:15 -0400

    comment out next.js
    to use next.js

---
+++ RetreiverApp/saas-landing-page/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/abdullahsufian/Documents/GitHub/5-8-EssentialsApp/RetreiverApp/saas-landing-page/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
